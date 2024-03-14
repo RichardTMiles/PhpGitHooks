1f474fcb372e3084ef50573dd87e2e9f73e4435a
richardtmiles <richard@miles.systems>
2024-03-14 00:31:13 -0600

    about to publish to packagist

--- .githooks/common.php
+++ .githooks/common.php
@@ -1,10 +1,36 @@
 <?php
 
+$autoloadPath = __DIR__;
 
-if (false === include __DIR__ . DIRECTORY_SEPARATOR . 'IdeaProjects' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php') {
-    // Load the autoload() for composer dependencies located in the Services folder
-    print '<h1>Try running <code>>> composer run rei</code></h1>';
-    die(1);
+$found = false;
+
+while (!$found && $autoloadPath !== DIRECTORY_SEPARATOR) { // Prevent infinite loop
+
+    $autoloadFile = $autoloadPath . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+    if (file_exists($autoloadFile)) {
+
+        $found = true;
+
+        break;
+
+    }
+
+    $autoloadPath = dirname($autoloadPath);
+
 }
 
+if ($found) {
+
+    require_once $autoloadFile;
+
+} else {
+
+    // Load the autoload() for composer dependencies located in the Services folder
+
+    print '<h1>Try running <code>composer install</code> from the root directory of your project.</h1>';
+
+    die(1);
+
+}
 
